7130bc62b916e741ab9c807680d5b9ce5a282629
Newton <5769156+iamnewton@users.noreply.github.com>
2021-04-05 13:23:03 -0700

    feat: ES Module prototype (#15)
    * feat: initial prototype
    
    * chore: delete gitkeep
    
    * feat: performance optimizations
    
    * fix: missing meta tag
    
    * chore(doc): clean up README
    
    * chore: reorganize into separate folder
    
    * feat: add in auction example

---
+++ prototypes/es-modules/auction/scripts/ssp.js
@@ -0,0 +1,16 @@
+export function score_ad (ad, bid, auction, scoring, browser) {
+	return bid * 10;
+}
+
+export function report_result(auction, browser) {
+	return {
+		foo: 'bar',
+		baz: 1,
+		qux: ['quux', 10, true ],
+		corge: {
+			grault: true,
+			garply: false,
+		},
+		waldo: true,
+	};
+}
