743d292bf82b4eaba4f9a5ed5b69ff95e018ce18
Newton Koumantzelis <mail@iamnewton.com>
2021-04-14 11:10:38 -0700

    chore: add utils unit tests

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,20 @@
 /* eslint-disable compat/compat */
+export const isMissingRequiredOptions = (obj, keys) => {
+	const missing = keys.filter(key => !obj.hasOwnProperty(key));
 
-export const validateTypes = {
+	if (missing.length) {
+		throw new Error(`Required options are missing! You must provide an 'object' of options with all of the following required options: ${missing.join(', ')}.`);
+	}
+
+	return false;
+};
+
+export const printInvalidOptionTypes = (options, invalid, types) => invalid.map(item => `'${item}' requires a type of "${types[item]}"! A type of ${typeof options[item]} was provided instead.`);
+
+export const validateType = {
 	array: arr => arr !== 'undefined' && Array.isArray(arr),
 	number: num => num !== 'undefined' && typeof num === 'number',
-	object: obj => obj !== 'undefined' && typeof obj === 'object' && obj !== null,
+	object: obj => obj !== 'undefined' && typeof obj === 'object' && obj !== null && !Array.isArray(obj),
 	string: str => str !== 'undefined' && typeof str === 'string',
 	url: url => {
 		try {
@@ -15,32 +26,20 @@ export const validateTypes = {
 };
 
 export const validateParam = (param, type) => {
-	const valid = validateTypes[type](param);
+	const valid = validateType[type](param);
 	if (!valid) {
 		throw new Error(`Must be of type "${type}"! ${typeof param} was provided.`);
 	}
 };
 
-export const printInvalidOptionTypes = (options, types) => types.forEach(item => `'${item}' requires a type of "${types[item]}"! A type of ${typeof options[item]} was provided instead.`);
-
 export const hasInvalidOptionTypes = (options, types) => {
 	const invalid = Object
 		.entries(options)
-		.filter(([ key, value ]) => !validateTypes[types[key]](value))
+		.filter(([ key, value ]) => !validateType[types[key]](value))
 		.flatMap(([ key ]) => key);
 
 	if (invalid.length) {
-		throw new Error(printInvalidOptionTypes(options, invalid).join('. '));
-	}
-
-	return false;
-};
-
-export const isMissingRequiredOptions = (obj, keys) => {
-	const missing = keys.filter(key => !obj.hasOwnProperty(key));
-
-	if (missing.length) {
-		throw new Error(`Required options are missing! You must provide an 'object' of options with all of the following required options: ${missing.join(', ')}.`);
+		throw new Error(printInvalidOptionTypes(options, invalid, types).join('. '));
 	}
 
 	return false;
