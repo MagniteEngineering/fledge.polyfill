1020be68a09c77d23306b8e021596d4a4ff4cb06
Newton Koumantzelis <mail@iamnewton.com>
2021-04-14 11:13:27 -0700

    fix: avoid calculating ms per day as per feedback

--- src/interest-groups/constants.js
+++ src/interest-groups/constants.js
@@ -1,8 +1,8 @@
-const hoursPerDay = 24;
-const minutesPerHour = 60;
-const secondsPerMinute = 60;
-const millisecondsPerSecond = 1000;
-export const MS_PER_DAY = hoursPerDay * minutesPerHour * secondsPerMinute * millisecondsPerSecond;
+// hours per day: 24;
+// minutes per hour: 60;
+// seconds per minute :60;
+// milliseconds per second: 1000;
+export const MS_PER_DAY = 86400000;
 
 export const MAX_DAYS = 30;
 export const MAX_EXPIRATION = MAX_DAYS * MS_PER_DAY;
