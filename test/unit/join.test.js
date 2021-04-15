153f11c5a11c25dcf8046f837f5b5dd92c48090e
Newton <5769156+iamnewton@users.noreply.github.com>
2021-04-14 17:02:29 -0700

    feat: `joinAdInterestGroup` & `leaveAdInterestGroup` functions (#31)
    * feat: initial checks for function parameters
    
    * fix: validate util passing incorrect arg
    
    * chore: add utils unit tests
    
    * fix: avoid calculating ms per day as per feedback
    
    * chore: clean up error message for clarity as per feedback
    
    * chore: clean up test language
    
    * chore: remove stub function
    
    * chore: remove all constants calculations
    
    * fix: import using relative path with file suffix
    
    * chore: add in jsdocs
    
    * chore: remove unnecessary constants abstraction file
    
    * chore: move tests out of source directory
    
    * chore: ignore mocks file from tests
    
    * chore: move unit tests to subdirectory
    
    * chore: create API docs
    
    * fix: main exports require file suffix

---
+++ test/unit/join.test.js
@@ -0,0 +1,120 @@
+import fledge from '../../src/';
+import {
+	mockAllOptions,
+	mockExpiry,
+	mockMaxExpiry,
+	mockOptionals,
+} from './mocks';
+
+describe('Interest Groups', () => {
+	describe('joinAdInterestGroup', () => {
+		describe('Errors', () => {
+			describe('function parameters', () => {
+				it('should throw an Error when no parameters are provided', () => {
+					expect(() => fledge.joinAdInterestGroup()).toThrow();
+				});
+
+				it('should throw an Error when no required options are provided', () => {
+					expect(() => fledge.joinAdInterestGroup({}, mockExpiry)).toThrow();
+					expect(() => fledge.joinAdInterestGroup(mockOptionals, mockExpiry)).toThrow();
+				});
+
+				describe('expiry parameter', () => {
+					it('should throw an Error when no expiry is provided', () => {
+						expect(() => fledge.joinAdInterestGroup(mockAllOptions)).toThrow();
+					});
+
+					it('should throw an Error when expiry is not a valid Number', () => {
+						expect(() => fledge.joinAdInterestGroup(mockAllOptions, 'mock')).toThrow();
+					});
+
+					it('should throw an Error when expiry is set beyond the maximum time allowed', () => {
+						expect(() => fledge.joinAdInterestGroup(mockAllOptions, 2 * mockMaxExpiry)).toThrow();
+					});
+				});
+
+				describe('data structures', () => {
+					it('should throw an Error when options is not a valid Object', () => {
+						expect(() => fledge.joinAdInterestGroup(undefined, mockExpiry)).toThrow();
+						expect(() => fledge.joinAdInterestGroup(true, mockExpiry)).toThrow();
+						expect(() => fledge.joinAdInterestGroup(0, mockExpiry)).toThrow();
+						expect(() => fledge.joinAdInterestGroup('mock', mockExpiry)).toThrow();
+						expect(() => fledge.joinAdInterestGroup(() => { /* noOp */ }, mockExpiry)).toThrow();
+						expect(() => fledge.joinAdInterestGroup(null, mockExpiry)).toThrow();
+					});
+
+					it("should throw an Error when 'owner' is not a valid String", () => {
+						const mockOptionsInvalidOwner = {
+							...mockAllOptions,
+							owner: 0,
+						};
+
+						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidOwner, mockExpiry)).toThrow();
+					});
+
+					it("should throw an Error when 'name' is not a valid String", () => {
+						const mockOptionsInvalidName = {
+							...mockAllOptions,
+							name: 0,
+						};
+						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidName, mockExpiry)).toThrow();
+					});
+
+					it("should throw an Error when 'bidding_logic_url' is not a valid URL", () => {
+						const mockOptionsInvalidBidding = {
+							...mockAllOptions,
+							bidding_logic_url: 'mock',
+						};
+						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidBidding, mockExpiry)).toThrow();
+					});
+
+					it("should throw an Error when 'daily_update_url' is not a valid URL", () => {
+						const mockOptionsInvalidDailyUpdate = {
+							...mockAllOptions,
+							daily_update_url: 'mock',
+						};
+						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidDailyUpdate, mockExpiry)).toThrow();
+					});
+
+					it("should throw an Error when 'trusted_bidding_signals_url' is not a valid URL", () => {
+						const mockOptionsInvalidTrustedSignalsUrl = {
+							...mockAllOptions,
+							trusted_bidding_signals_url: 'mock',
+						};
+						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidTrustedSignalsUrl, mockExpiry)).toThrow();
+					});
+
+					it("should throw an Error when 'trusted_bidding_signals_keys' is not a valid Array of Strings", () => {
+						const mockOptionsInvalidTrustedSignalsKeys = {
+							...mockAllOptions,
+							trusted_bidding_signals_keys: 'mock',
+						};
+						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidTrustedSignalsKeys, mockExpiry)).toThrow();
+					});
+
+					it("should throw an Error when 'user_bidding_signals' is not a valid Object", () => {
+						const mockOptionsInvalidUserSignals = {
+							...mockAllOptions,
+							user_bidding_signals: 'mock',
+						};
+						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidUserSignals, mockExpiry)).toThrow();
+					});
+
+					it("should throw an Error when 'ads' is not a valid Array", () => {
+						const mockOptionsInvalidAds = {
+							...mockAllOptions,
+							ads: 'mock',
+						};
+						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidAds, mockExpiry)).toThrow();
+					});
+				});
+			});
+		});
+
+		describe('Return', () => {
+			it('should return true when all valid options are provided', () => {
+				expect(fledge.joinAdInterestGroup(mockAllOptions, mockExpiry)).toBe(true);
+			});
+		});
+	});
+});
---
+++ test/unit/leave.test.js
@@ -0,0 +1,57 @@
+import fledge from '../../src/';
+import {
+	mockAllOptions,
+	mockOptionals,
+} from './mocks';
+
+describe('Interest Groups', () => {
+	describe('leaveAdInterestGroup', () => {
+		describe('Errors', () => {
+			describe('function parameters', () => {
+				it('should throw an Error when no options are provided', () => {
+					expect(() => fledge.leaveAdInterestGroup()).toThrow();
+				});
+
+				describe('required option parameters', () => {
+					it('should throw an Error when no required options are provided', () => {
+						expect(() => fledge.leaveAdInterestGroup({})).toThrow();
+						expect(() => fledge.leaveAdInterestGroup(mockOptionals)).toThrow();
+					});
+				});
+
+				describe('data structures', () => {
+					it('should throw an Error when options is not a valid Object', () => {
+						expect(() => fledge.leaveAdInterestGroup(undefined)).toThrow();
+						expect(() => fledge.leaveAdInterestGroup(true)).toThrow();
+						expect(() => fledge.leaveAdInterestGroup(0)).toThrow();
+						expect(() => fledge.leaveAdInterestGroup('mock')).toThrow();
+						expect(() => fledge.leaveAdInterestGroup(() => { /* noOp */ })).toThrow();
+						expect(() => fledge.leaveAdInterestGroup(null)).toThrow();
+					});
+
+					it("should throw an Error when 'owner' is not a valid String", () => {
+						const mockOptionsInvalidOwner = {
+							...mockAllOptions,
+							owner: 0,
+						};
+						expect(() => fledge.leaveAdInterestGroup(mockOptionsInvalidOwner)).toThrow();
+					});
+
+					it("should throw an Error when 'name' is not a valid String", () => {
+						const mockOptionsInvalidName = {
+							...mockAllOptions,
+							name: 0,
+						};
+						expect(() => fledge.leaveAdInterestGroup(mockOptionsInvalidName)).toThrow();
+					});
+				});
+			});
+		});
+
+		describe('Return', () => {
+			it('should return true when all valid options are provided', () => {
+				expect(fledge.leaveAdInterestGroup(mockAllOptions)).toBe(true);
+			});
+		});
+	});
+});
