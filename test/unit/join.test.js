a2b5f939217e9b0fc02356aa4b592b7811a7b52d
Newton Koumantzelis <mail@iamnewton.com>
2021-04-16 14:54:43 -0700

    chore: fix async tests

--- test/unit/join.test.js
+++ test/unit/join.test.js
@@ -10,110 +10,111 @@ describe('Interest Groups', () => {
 	describe('joinAdInterestGroup', () => {
 		describe('Errors', () => {
 			describe('function parameters', () => {
-				it('should throw an Error when no parameters are provided', () => {
-					expect(() => fledge.joinAdInterestGroup()).toThrow();
+				it('should throw an Error when no parameters are provided', async () => {
+					await expect(() => fledge.joinAdInterestGroup()).rejects.toThrow();
 				});
 
-				it('should throw an Error when no required options are provided', () => {
-					expect(() => fledge.joinAdInterestGroup({}, mockExpiry)).toThrow();
-					expect(() => fledge.joinAdInterestGroup(mockOptionals, mockExpiry)).toThrow();
+				it('should throw an Error when no required options are provided', async () => {
+					await expect(() => fledge.joinAdInterestGroup({}, mockExpiry)).rejects.toThrow();
+					await expect(() => fledge.joinAdInterestGroup(mockOptionals, mockExpiry)).rejects.toThrow();
 				});
 
 				describe('expiry parameter', () => {
-					it('should throw an Error when no expiry is provided', () => {
-						expect(() => fledge.joinAdInterestGroup(mockAllOptions)).toThrow();
+					it('should throw an Error when no expiry is provided', async () => {
+						await expect(() => fledge.joinAdInterestGroup(mockAllOptions)).rejects.toThrow();
 					});
 
-					it('should throw an Error when expiry is not a valid Number', () => {
-						expect(() => fledge.joinAdInterestGroup(mockAllOptions, 'mock')).toThrow();
+					it('should throw an Error when expiry is not a valid Number', async () => {
+						await expect(() => fledge.joinAdInterestGroup(mockAllOptions, 'mock')).rejects.toThrow();
 					});
 
-					it('should throw an Error when expiry is set beyond the maximum time allowed', () => {
-						expect(() => fledge.joinAdInterestGroup(mockAllOptions, 2 * mockMaxExpiry)).toThrow();
+					it('should throw an Error when expiry is set beyond the maximum time allowed', async () => {
+						await expect(() => fledge.joinAdInterestGroup(mockAllOptions, 2 * mockMaxExpiry)).rejects.toThrow();
 					});
 				});
 
 				describe('data structures', () => {
-					it('should throw an Error when options is not a valid Object', () => {
-						expect(() => fledge.joinAdInterestGroup(undefined, mockExpiry)).toThrow();
-						expect(() => fledge.joinAdInterestGroup(true, mockExpiry)).toThrow();
-						expect(() => fledge.joinAdInterestGroup(0, mockExpiry)).toThrow();
-						expect(() => fledge.joinAdInterestGroup('mock', mockExpiry)).toThrow();
-						expect(() => fledge.joinAdInterestGroup(() => { /* noOp */ }, mockExpiry)).toThrow();
-						expect(() => fledge.joinAdInterestGroup(null, mockExpiry)).toThrow();
+					it('should throw an Error when options is not a valid Object', async () => {
+						await expect(() => fledge.joinAdInterestGroup(undefined, mockExpiry)).rejects.toThrow();
+						await expect(() => fledge.joinAdInterestGroup(true, mockExpiry)).rejects.toThrow();
+						await expect(() => fledge.joinAdInterestGroup(0, mockExpiry)).rejects.toThrow();
+						await expect(() => fledge.joinAdInterestGroup('mock', mockExpiry)).rejects.toThrow();
+						await expect(() => fledge.joinAdInterestGroup(() => { /* noOp */ }, mockExpiry)).rejects.toThrow();
+						await expect(() => fledge.joinAdInterestGroup(null, mockExpiry)).rejects.toThrow();
 					});
 
-					it("should throw an Error when 'owner' is not a valid String", () => {
+					it("should throw an Error when 'owner' is not a valid String", async () => {
 						const mockOptionsInvalidOwner = {
 							...mockAllOptions,
 							owner: 0,
 						};
 
-						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidOwner, mockExpiry)).toThrow();
+						await expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidOwner, mockExpiry)).rejects.toThrow();
 					});
 
-					it("should throw an Error when 'name' is not a valid String", () => {
+					it("should throw an Error when 'name' is not a valid String", async () => {
 						const mockOptionsInvalidName = {
 							...mockAllOptions,
 							name: 0,
 						};
-						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidName, mockExpiry)).toThrow();
+						await expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidName, mockExpiry)).rejects.toThrow();
 					});
 
-					it("should throw an Error when 'bidding_logic_url' is not a valid URL", () => {
+					it("should throw an Error when 'bidding_logic_url' is not a valid URL", async () => {
 						const mockOptionsInvalidBidding = {
 							...mockAllOptions,
 							bidding_logic_url: 'mock',
 						};
-						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidBidding, mockExpiry)).toThrow();
+						await expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidBidding, mockExpiry)).rejects.toThrow();
 					});
 
-					it("should throw an Error when 'daily_update_url' is not a valid URL", () => {
+					it("should throw an Error when 'daily_update_url' is not a valid URL", async () => {
 						const mockOptionsInvalidDailyUpdate = {
 							...mockAllOptions,
 							daily_update_url: 'mock',
 						};
-						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidDailyUpdate, mockExpiry)).toThrow();
+						await expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidDailyUpdate, mockExpiry)).rejects.toThrow();
 					});
 
-					it("should throw an Error when 'trusted_bidding_signals_url' is not a valid URL", () => {
+					it("should throw an Error when 'trusted_bidding_signals_url' is not a valid URL", async () => {
 						const mockOptionsInvalidTrustedSignalsUrl = {
 							...mockAllOptions,
 							trusted_bidding_signals_url: 'mock',
 						};
-						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidTrustedSignalsUrl, mockExpiry)).toThrow();
+						await expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidTrustedSignalsUrl, mockExpiry)).rejects.toThrow();
 					});
 
-					it("should throw an Error when 'trusted_bidding_signals_keys' is not a valid Array of Strings", () => {
+					it("should throw an Error when 'trusted_bidding_signals_keys' is not a valid Array of Strings", async () => {
 						const mockOptionsInvalidTrustedSignalsKeys = {
 							...mockAllOptions,
 							trusted_bidding_signals_keys: 'mock',
 						};
-						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidTrustedSignalsKeys, mockExpiry)).toThrow();
+						await expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidTrustedSignalsKeys, mockExpiry)).rejects.toThrow();
 					});
 
-					it("should throw an Error when 'user_bidding_signals' is not a valid Object", () => {
+					it("should throw an Error when 'user_bidding_signals' is not a valid Object", async () => {
 						const mockOptionsInvalidUserSignals = {
 							...mockAllOptions,
 							user_bidding_signals: 'mock',
 						};
-						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidUserSignals, mockExpiry)).toThrow();
+						await expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidUserSignals, mockExpiry)).rejects.toThrow();
 					});
 
-					it("should throw an Error when 'ads' is not a valid Array", () => {
+					it("should throw an Error when 'ads' is not a valid Array", async () => {
 						const mockOptionsInvalidAds = {
 							...mockAllOptions,
 							ads: 'mock',
 						};
-						expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidAds, mockExpiry)).toThrow();
+						await expect(() => fledge.joinAdInterestGroup(mockOptionsInvalidAds, mockExpiry)).rejects.toThrow();
 					});
 				});
 			});
 		});
 
 		describe('Return', () => {
-			it('should return true when all valid options are provided', () => {
-				expect(fledge.joinAdInterestGroup(mockAllOptions, mockExpiry)).toBe(true);
+			it('should return true when all valid options are provided', async () => {
+				const ig = await fledge.joinAdInterestGroup(mockAllOptions, mockExpiry);
+				await expect(ig).toBe(true);
 			});
 		});
 	});
