8b2afe1a5bb52b45f061bb69be6674e87d1f4cfe
Newton Koumantzelis <mail@iamnewton.com>
2021-04-18 12:15:00 -0700

    fix: serve rewrites files, fix by renaming index to test

--- test/integration/interest-groups/index.test.js
+++ test/integration/interest-groups/index.test.js
@@ -2,7 +2,7 @@
 
 describe('Initial test', () => {
 	beforeAll(async () => {
-		await page.goto('http://localhost:3000/test/integration/interest-groups/index.html');
+		await page.goto('http://localhost:3000/test/integration/interest-groups/test.html');
 	});
 
 	it('should be titled "interest-groups.test"', async () => {
