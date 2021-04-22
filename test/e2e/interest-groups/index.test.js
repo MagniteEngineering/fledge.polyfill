5c79b3a2af5ba1b27d97e2c7d0121720d7b53276
Newton Koumantzelis <mail@iamnewton.com>
2021-04-22 16:36:22 -0700

    chore: use `toThrow`

--- test/e2e/interest-groups/index.test.js
+++ test/e2e/interest-groups/index.test.js
@@ -1,52 +1,24 @@
 /* eslint-disable no-undef */
 
-describe('Initial test', () => {
-	beforeAll(async () => {
-		await page.goto('http://localhost:3000/test/e2e/interest-groups/');
-	});
-
-	it('should be titled "interest-groups.test"', async () => {
-		await expect(await page.title()).toMatch('interest-groups.test');
-	});
-
-	it('should provide the fledge code', async () => {
-		await expect(await page.title()).toMatch('interest-groups.test');
-		const fledge = await page.evaluate(() => window.fledge.fledge);
-		expect(Object.keys(fledge)).toEqual([ 'joinAdInterestGroup', 'leaveAdInterestGroup' ]);
-	});
-});
-
 describe('joinAdInterestGroup', () => {
 	beforeEach(async () => {
 		await page.goto('http://localhost:3000/test/e2e/interest-groups/');
 	});
 
 	it('should error when no parameters sent', async () => {
-		let errorThrown = false;
-		try {
-			await page.evaluate(() =>
-				window.fledge.fledge.joinAdInterestGroup(),
-			);
-		} catch (e) {
-			errorThrown = true;
-		}
-		expect(errorThrown).toBe(true);
+		const fledge = await page.evaluate(() => window.fledge.fledge);
+		expect(() => fledge.joinAdInterestGroup()).toThrow();
 	});
 
-	it('should not error went provided minimum required params', async () => {
-		let errorThrown = false;
-		try {
-			await page.evaluate(() =>
-				window.fledge.fledge.joinAdInterestGroup({
-					owner: 'magnite.com',
-					name: 'test-interest',
-					bidding_logic_url: 'https://fledge.magnite.com/bl.js',
-				}, 60000),
-			);
-		} catch (e) {
-			errorThrown = true;
-		}
-		expect(errorThrown).toBe(false);
+	it('should return true when provided minimum required params', async () => {
+		const fledge = await page.evaluate(() =>
+			window.fledge.fledge.joinAdInterestGroup({
+				owner: 'magnite.com',
+				name: 'test-interest',
+				bidding_logic_url: 'https://fledge.magnite.com/bl.js',
+			}, 60000),
+		);
+		expect(fledge).toBe(true);
 	});
 
 	it('should store expected data in local storage', async () => {
