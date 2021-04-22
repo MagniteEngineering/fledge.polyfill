ac63953e9885d6720afcfeccbc5318fe3dd0d0eb
Brad Rodriguez <brodriguez@rubiconproject.com>
2021-04-22 12:50:46 -0700

    test: testing indexedDB storage

--- test/e2e/interest-groups/index.test.js
+++ test/e2e/interest-groups/index.test.js
@@ -48,4 +48,34 @@ describe('joinAdInterestGroup', () => {
 		}
 		expect(errorThrown).toBe(false);
 	});
+
+	it('should store expected data in local storage', async () => {
+		const igObject = {
+			owner: 'magnite.com',
+			name: 'test-interest',
+			bidding_logic_url: 'https://fledge.magnite.com/bl.js',
+		};
+		const expiry = 60000;
+		await page.evaluate((igObject, expiry) =>
+			window.fledge.fledge.joinAdInterestGroup(igObject, expiry),
+		igObject,
+		expiry,
+		);
+		const result = await page.evaluate(() => {
+			// eslint-disable-next-line
+			const myPromise = new Promise((resolve, reject) => {
+				const request = window.indexedDB.open('Fledge');
+				request.onsuccess = () => {
+					const db = request.result;
+					db.transaction('interest-groups', 'readonly').objectStore('interest-groups').get('magnite.com-test-interest').onsuccess = function (event) {
+						resolve(event.target.result);
+					};
+				};
+			});
+			return myPromise;
+		});
+		expect(result.owner).toBe(igObject.owner);
+		expect(result.name).toBe(igObject.name);
+		expect(result.bidding_logic_url).toBe(igObject.bidding_logic_url);
+	});
 });
