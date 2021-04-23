52c93750d78b5cc718fbe013292fb62f429f91f2
Newton <5769156+iamnewton@users.noreply.github.com>
2021-04-23 16:06:25 -0700

    feat: `runAdAuction` (#45)

--- test/e2e/interest-groups/index.test.js
+++ test/e2e/interest-groups/index.test.js
@@ -1,53 +1,50 @@
 /* eslint-disable no-undef */
+describe('Fledge', () => {
+	describe('joinAdInterestGroup', () => {
+		beforeEach(async () => {
+			await page.goto('http://localhost:3000/test/e2e/interest-groups/');
+		});
 
-describe('joinAdInterestGroup', () => {
-	beforeEach(async () => {
-		await page.goto('http://localhost:3000/test/e2e/interest-groups/');
-	});
+		it('should error when no parameters sent', async () => {
+			const fledge = await page.evaluate(() => window.fledge.fledge);
+			expect(() => fledge.joinAdInterestGroup()).toThrow();
+		});
 
-	it('should error when no parameters sent', async () => {
-		const fledge = await page.evaluate(() => window.fledge.fledge);
-		expect(() => fledge.joinAdInterestGroup()).toThrow();
-	});
+		it('should return true when provided minimum required params', async () => {
+			const result = await page.evaluate(() =>
+				window.fledge.fledge.joinAdInterestGroup({
+					owner: 'magnite.com',
+					name: 'test-interest',
+					bidding_logic_url: 'https://fledge.magnite.com/bl.js',
+				}, 60000),
+			);
+			expect(result).toBe(true);
+		});
 
-	it('should return true when provided minimum required params', async () => {
-		const result = await page.evaluate(() =>
-			window.fledge.fledge.joinAdInterestGroup({
+		it('should store expected data in local storage', async () => {
+			const igObject = {
 				owner: 'magnite.com',
 				name: 'test-interest',
 				bidding_logic_url: 'https://fledge.magnite.com/bl.js',
-			}, 60000),
-		);
-		expect(result).toBe(true);
-	});
-
-	it('should store expected data in local storage', async () => {
-		const igObject = {
-			owner: 'magnite.com',
-			name: 'test-interest',
-			bidding_logic_url: 'https://fledge.magnite.com/bl.js',
-		};
-		const expiry = 60000;
-		await page.evaluate((igObject, expiry) =>
-			window.fledge.fledge.joinAdInterestGroup(igObject, expiry),
-		igObject,
-		expiry,
-		);
-		const result = await page.evaluate(() => {
-			// eslint-disable-next-line
-			const myPromise = new Promise((resolve, reject) => {
-				const request = window.indexedDB.open('Fledge');
-				request.onsuccess = () => {
-					const db = request.result;
-					db.transaction('interest-groups', 'readonly').objectStore('interest-groups').get('magnite.com-test-interest').onsuccess = function (event) {
-						resolve(event.target.result);
+			};
+			const expiry = 60000;
+			await page.evaluate((igObject, expiry) => window.fledge.joinAdInterestGroup(igObject, expiry), igObject, expiry);
+			const result = await page.evaluate(() => {
+				// eslint-disable-next-line
+				const myPromise = new Promise((resolve, reject) => {
+					const request = window.indexedDB.open('Fledge');
+					request.onsuccess = () => {
+						const db = request.result;
+						db.transaction('interest-groups', 'readonly').objectStore('interest-groups').get('magnite.com-test-interest').onsuccess = function (event) {
+							resolve(event.target.result);
+						};
 					};
-				};
+				});
+				return myPromise;
 			});
-			return myPromise;
+			expect(result.owner).toBe(igObject.owner);
+			expect(result.name).toBe(igObject.name);
+			expect(result.bidding_logic_url).toBe(igObject.bidding_logic_url);
 		});
-		expect(result.owner).toBe(igObject.owner);
-		expect(result.name).toBe(igObject.name);
-		expect(result.bidding_logic_url).toBe(igObject.bidding_logic_url);
 	});
 });
