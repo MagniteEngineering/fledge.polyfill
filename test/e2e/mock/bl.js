504ef88cd90324baa0a39dc7bf171d6a299f192c
Brad Rodriguez <brodriguez@rubiconproject.com>
2021-05-06 17:16:16 -0700

    chore: adding test for runAdAuction (#54)

---
+++ test/e2e/mock/bl.js
@@ -0,0 +1,11 @@
+/* eslint-disable camelcase */
+export const generate_bid = (interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) => {
+	console.log('generating bid...');
+	return {
+		ad: {
+			good: 'stuff',
+		},
+		bid: 1,
+		render: 'http://url.com',
+	};
+};
