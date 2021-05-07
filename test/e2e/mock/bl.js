e8db17290bda022cb091fe9d1da8458d9554a579
Newton Koumantzelis <mail@iamnewton.com>
2021-05-06 17:34:16 -0700

    chore: change to camel case

--- test/e2e/mock/bl.js
+++ test/e2e/mock/bl.js
@@ -1,11 +1,8 @@
 /* eslint-disable camelcase */
-export const generate_bid = (interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) => {
-	console.log('generating bid...');
-	return {
-		ad: {
-			good: 'stuff',
-		},
-		bid: 1,
-		render: 'http://url.com',
-	};
-};
+export const generateBid = (interestGroup, auctionSignals, perBuyerSignals, trustedBiddingSignals, browserSignals) => ({
+	ad: {
+		good: 'stuff',
+	},
+	bid: 1,
+	render: 'http://url.com',
+});
--- test/e2e/mock/dl.js
+++ test/e2e/mock/dl.js
@@ -1,5 +1,2 @@
 /* eslint-disable camelcase */
-export const score_ad = (adMetadata, bid, auctionConfig, trustedScoringSignals, browserSignals) => {
-	console.log('scoring bid...');
-	return bid;
-};
+export const scoreAd = (adMetadata, bid, auctionConfig, trustedScoringSignals, browserSignals) => bid;
