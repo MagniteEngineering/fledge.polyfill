45abc00b597389d1a186b7baa46a4bf11319c2fe
Brad Rodriguez <brodriguez@rubiconproject.com>
2021-05-11 14:00:21 -0700

    test: leveraging a more explicit promise

--- test/e2e/auction/index.test.js
+++ test/e2e/auction/index.test.js
@@ -22,44 +22,47 @@ describe('Fledge', () => {
 
 			const result = await page.evaluate(() => {
 				const fledge = new window.fledge();
-				return fledge.runAdAuction({
-					seller: 'publisher.example',
-					decision_logic_url: 'http://localhost:3000/test/e2e/mock/dl.js',
-					trusted_scoring_signals_url: 'http://localhost:3000/test/e2e/tss/',
-					interest_group_buyers: '*',
-					additional_bids: [
-						{
-							price: 1,
-							class: 'deal',
-						},
-					],
-					auction_signals: {
-						size: {
-							w: 300,
-							h: 200,
-						},
-						content: [
-							'news',
-							'politics',
-							'us',
-							'election',
+				return new Promise((resolve, reject) => {
+					fledge.runAdAuction({
+						seller: 'publisher.example',
+						decision_logic_url: 'http://localhost:3000/test/e2e/mock/dl.js',
+						trusted_scoring_signals_url: 'http://localhost:3000/test/e2e/tss/',
+						interest_group_buyers: '*',
+						additional_bids: [
+							{
+								price: 1,
+								class: 'deal',
+							},
 						],
-						location: 'atf',
-					},
-					seller_signals: {
-						account_id: 1234,
-						site_id: 1234,
-						zone_id: 1234,
-						size_id: 123,
-					},
-					per_buyer_signals: {
-						'dsp.com': {
-							content_quality: 230,
+						auction_signals: {
+							size: {
+								w: 300,
+								h: 200,
+							},
+							content: [
+								'news',
+								'politics',
+								'us',
+								'election',
+							],
+							location: 'atf',
 						},
-					},
+						seller_signals: {
+							account_id: 1234,
+							site_id: 1234,
+							zone_id: 1234,
+							size_id: 123,
+						},
+						per_buyer_signals: {
+							'dsp.com': {
+								content_quality: 230,
+							},
+						},
+					}).then(result => {
+						resolve(result);
+					});
 				});
 			});
-			console.log(result);
 			expect(typeof result).toBe('string');
 		});
 
