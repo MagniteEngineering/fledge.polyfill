cd3096702e4c74a84c774bad3875d8a22956220d
Brad Rodriguez <brodrigu@gmail.com>
2021-05-18 16:18:37 -0700

    feat: adding simple performance loops

--- test/perf/join-ig.js
+++ test/perf/join-ig.js
@@ -6,35 +6,30 @@ const microtime = require('microtime');
 module.exports = async () => {
 	console.log('starting performance test for joinAdInterestGroup');
 
-	const numInterestGroups = 1;
+	const numInterestGroups = 100;
 
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto('http://localhost:3000/test/e2e/');
 	let totalExecutionTime = 0;
-	console.log(1);
 	for (let i = 0; i < numInterestGroups; i++) {
 		const name = randomstring.generate(7);
 		const start = microtime.now();
-		console.log(2);
 		await page.evaluate(name => {
-			const fledge = new window.fledge();
+			const fledge = new window.fledge('http://localhost:3000/docs/iframe.html');
 			return fledge.joinAdInterestGroup({
 				owner: 'magnite.com',
 				name,
 				biddingLogicUrl: 'http://localhost:3000/test/mocks/bl.js',
 			}, 60000);
 		}, name);
-		console.log(3);
 		const end = microtime.now();
 		totalExecutionTime += end - start;
 	}
-	console.log(4);
 
 	console.log(`total time for ${numInterestGroups} calls: ${totalExecutionTime}µs`);
 	console.log(`average call time: ${totalExecutionTime / numInterestGroups}µs`);
 	await browser.close();
-	console.log(5);
 	console.log('performance test complete');
 };
 
--- test/perf/run-auction.js
+++ test/perf/run-auction.js
@@ -7,7 +7,7 @@ const joinAdInterestGroups = async (numGroups, page) => {
 	for (let i = 0; i < numGroups; i++) {
 		const name = randomstring.generate(7);
 		await page.evaluate(name => {
-			const fledge = new window.fledge();
+			const fledge = new window.fledge('http://localhost:3000/docs/iframe.html');
 			return fledge.joinAdInterestGroup({
 				owner: 'magnite.com',
 				name,
@@ -17,66 +17,73 @@ const joinAdInterestGroups = async (numGroups, page) => {
 	}
 };
 
-module.exports = async () => {
-	const numInterestGroups = 1;
-	const numAuctions = 1;
+const runAuction = async page => {
+	const start = microtime.now();
+	await page.evaluate(() => {
+		const fledge = new window.fledge('http://localhost:3000/docs/iframe.html');
+		return fledge.runAdAuction({
+			seller: 'publisher.example',
+			decisionLogicUrl: 'http://localhost:3000/test/mocks/dl.js',
+			trustedScoringSignalsUrl: 'http://localhost:3000/test/e2e/tss/',
+			interestGroupBuyers: '*',
+			additionalBids: [
+				{
+					price: 1,
+					class: 'deal',
+				},
+			],
+			auctionSignals: {
+				size: {
+					w: 300,
+					h: 200,
+				},
+				content: [
+					'news',
+					'politics',
+					'us',
+					'election',
+				],
+				location: 'atf',
+			},
+			sellerSignals: {
+				account_id: 1234,
+				site_id: 1234,
+				zone_id: 1234,
+				size_id: 123,
+			},
+			perBuyerSignals: {
+				'dsp.com': {
+					content_quality: 230,
+				},
+			},
+		});
+	});
+	return microtime.now() - start;
+};
 
-	console.log(`starting performance test for runAdAuction with ${numInterestGroups} interest groups.`);
-
+const runTest = async numInterestGroups => {
+	const numAuctions = 10;
 	const browser = await puppeteer.launch();
 	const context = await browser.createIncognitoBrowserContext();
 	const page = await context.newPage();
 	await page.goto('http://localhost:3000/test/e2e/');
 
 	await joinAdInterestGroups(numInterestGroups, page);
-
 	let totalExecutionTime = 0;
 	for (let i = 0; i < numAuctions; i++) {
-		const start = microtime.now();
-		await page.evaluate(() => {
-			const fledge = new window.fledge();
-			return fledge.runAdAuction({
-				seller: 'publisher.example',
-				decisionLogicUrl: 'http://localhost:3000/test/mocks/dl.js',
-				trustedScoringSignalsUrl: 'http://localhost:3000/test/e2e/tss/',
-				interestGroupBuyers: '*',
-				additionalBids: [
-					{
-						price: 1,
-						class: 'deal',
-					},
-				],
-				auctionSignals: {
-					size: {
-						w: 300,
-						h: 200,
-					},
-					content: [
-						'news',
-						'politics',
-						'us',
-						'election',
-					],
-					location: 'atf',
-				},
-				sellerSignals: {
-					account_id: 1234,
-					site_id: 1234,
-					zone_id: 1234,
-					size_id: 123,
-				},
-				perBuyerSignals: {
-					'dsp.com': {
-						content_quality: 230,
-					},
-				},
-			});
-		});
-		const end = microtime.now();
-		totalExecutionTime += end - start;
+		totalExecutionTime += await runAuction(page);
 	}
-	console.log(`total time for ${numAuctions} calls: ${totalExecutionTime}µs`);
-	console.log(`average call time: ${totalExecutionTime / numInterestGroups}µs`);
-	await browser.close();
-	console.log('performance test complete');
+	return totalExecutionTime / numAuctions;
+};
+
+module.exports = async () => {
+	console.log(`starting performance test for runAdAuction`);
+	const maxInterestGroups = 100;
+	const results = {};
+	for (let i = 1; i < maxInterestGroups; i += 10) {
+		console.log(`starting performance test for runAdAuction with ${i} interest groups.`);
+		results[i] = await runTest(i);
+		console.log(`average call time: ${results[i]}µs`);
+		console.log('performance test complete');
+	}
 };
